88c2c5e5cec279271f46324607c41ba4882493ba
Joykishan Sharma <joykishan120@gmail.com>
2020-09-13 13:00:35 +0530

    added helloWorld files (#7)
    Added contribution from @JoykishanSharma

---
+++ Contributors/Joykishan Sharma/helloWorld.c++
@@ -0,0 +1,8 @@
+//to run c/c++ files, your machine should have 'gcc' installed
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!";
+    return 0;
+}
---
+++ Contributors/Joykishan Sharma/helloWorld.java
@@ -0,0 +1,9 @@
+//To print in console, type 'java <filename_with_extension>'
+//In our case, it would be 'java helloWorld.java'
+//If you got error, it might be because your machine don't have 'jdk' installed
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
