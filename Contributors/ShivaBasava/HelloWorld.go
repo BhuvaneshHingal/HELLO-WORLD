972c4c34ff79d5788b12c1f5777b8c7dd5485360
shiva47 <36756990+ShivaBasava@users.noreply.github.com>
2020-10-02 15:57:40 +0530

    Added files for @ShivaBasava (#27)

---
+++ Contributors/ShivaBasava/HelloWorld.go
@@ -0,0 +1,17 @@
+package main //Package Declaration
+
+//Importing fmt Package, used for formatted Input/Output Operations
+import "fmt"
+
+//main() , An Entry point to print - Hello, World!
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+/*
+
+@console to execute the program,
+
+go run ./HelloWorld.go
+
+*/
