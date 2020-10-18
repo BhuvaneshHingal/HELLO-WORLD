f2d4debf03b1a0be55c75f5d22f1b78c03ea6b1d
HONG QI XING <qixing1994@hotmail.com>
2020-10-19 00:26:50 +0800

    Create hello.java (#71)
    hello world in java

---
+++ Contributors/luchong/hello.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+     public static void main(String []args){
+        String[] text = {"Hello", "World"};
+        System.out.println(text[0] + " " + text[1]);
+     }
+}
