b1ce27fc47b381b3acd68c701a5bbf6df652258d
bidishaMondal7 <bidisha.tuli7@gmail.com>
2021-10-01 13:34:52 +0530

    HELLO WORLD with ruby

---
+++ Contributors/BidishaBM7/hello.rb
@@ -0,0 +1,21 @@
+=begin 
+    Program to print Hello world
+=end
+
+# Assigning corresponding ascii value as array
+first = [72, 69, 76, 76, 79]
+last = [87, 79, 82, 76, 68]
+
+#converting ascii to character
+str1 = first.pack('c* ')
+str2 = last.pack('c*')
+
+ #Printing HELLO WORLD!
+puts str1 + ' ' + str2
+
+
+
+
+
+
+
