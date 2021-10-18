89b3afcaa33354ca665e636063f2c6643179f16e
Ashhar Farooqui <nazzfarooqui22@gmail.com>
2021-10-18 22:50:21 +0530

    This commit is for hacktoberFest 2021

---
+++ Contributors/Ashhar/Hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>	//Pre-processor directive
+int main()		//main function declaration
+{
+printf("Hello World");	//to output the string on a display
+return 0;		//terminating function
+}
